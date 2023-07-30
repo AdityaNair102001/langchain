318e1d177b58b24baa50ab2bec609363b25b502c
AdityaNair102001 <72807512+AdityaNair102001@users.noreply.github.com>
2023-07-31 02:34:34 +0530

    chore: removed the key

--- index.js
+++ index.js
@@ -4,26 +4,30 @@ import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import * as fs from "fs";
 import PDFParser from "pdf2json";
 
-const pdfParser = new PDFParser(this, 1);
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+const loader = new PDFLoader("./resume.pdf");
+const docs = await loader.load();
 
-pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError));
-pdfParser.on("pdfParser_dataReady", pdfData => {
-    fs.writeFile("./content.txt", pdfParser.getRawTextContent(), () => { console.log("Done."); });
-});
+// const pdfParser = new PDFParser(this, 1);
 
-pdfParser.loadPDF("./resume.pdf");
+// pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError));
+// pdfParser.on("pdfParser_dataReady", pdfData => {
+//     fs.writeFile("./content.txt", pdfParser.getRawTextContent(), () => { console.log("Done."); });
+// });
+
+// pdfParser.loadPDF("./resume.pdf");
 
 
 
 
 
 // In this example, we use a `MapReduceDocumentsChain` specifically prompted to summarize a set of documents.
-const text = fs.readFileSync("./content.txt", "utf8");
+// const text = fs.readFileSync("./content.txt", "utf8");
 
 
-const model = new OpenAI({ openAIApiKey: "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", temperature: 0 });
-const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
-const docs = await textSplitter.createDocuments([text]);
+const model = new OpenAI({ openAIApiKey: "", temperature: 0 });
+// const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
+// const docs = await textSplitter.createDocuments([text]);
 
 // This convenience function creates a document chain prompted to summarize a set of documents.
 const chain = loadSummarizationChain(model, { type: "map_reduce" });
