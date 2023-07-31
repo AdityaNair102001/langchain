ec88fa8116a0882a61af51348d0234ef062abc80
AdityaNair102001 <72807512+AdityaNair102001@users.noreply.github.com>
2023-07-31 09:39:21 +0530

    feat: assignment complete

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# langchain
+# Langchain PDF Summarizer
  
+### How to run:
+- run `npm install` in console
+- set environment variable for `OPENAI_API_KEY` in .env file.
+- run `node index.js` in console.
--- index.js
+++ index.js
@@ -1,33 +1,13 @@
+import 'dotenv/config'
 import { OpenAI } from "langchain/llms/openai";
 import { loadSummarizationChain } from "langchain/chains";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import * as fs from "fs";
-import PDFParser from "pdf2json";
-
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
-const loader = new PDFLoader("./resume.pdf");
+
+const loader = new PDFLoader("./dummy.pdf");
 const docs = await loader.load();
 
-// const pdfParser = new PDFParser(this, 1);
+const model = new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY, temperature: 0 });
 
-// pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError));
-// pdfParser.on("pdfParser_dataReady", pdfData => {
-//     fs.writeFile("./content.txt", pdfParser.getRawTextContent(), () => { console.log("Done."); });
-// });
-
-// pdfParser.loadPDF("./resume.pdf");
-
-
-
-
-
-// In this example, we use a `MapReduceDocumentsChain` specifically prompted to summarize a set of documents.
-// const text = fs.readFileSync("./content.txt", "utf8");
-
-
-const model = new OpenAI({ openAIApiKey: "", temperature: 0 });
-// const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
-// const docs = await textSplitter.createDocuments([text]);
 
 // This convenience function creates a document chain prompted to summarize a set of documents.
 const chain = loadSummarizationChain(model, { type: "map_reduce" });
